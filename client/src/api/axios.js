df1b9d9e936640c0c7cd3849071293fed8b7fe66
yaswanthm2909 <yaswanthm2909@gmail.com>
2026-03-19 10:53:29 +0530

    update backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ott-platform-vfba.onrender.com/api",
+  baseURL: "https://ott-platform-server-mwyo.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
